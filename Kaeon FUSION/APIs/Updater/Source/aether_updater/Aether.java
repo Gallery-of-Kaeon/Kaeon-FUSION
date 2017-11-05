e1c9a7b5ec88a41bb3432d11bd3ae5c3cccf187b
Kipmon <mdinkin54@gmail.com>
2017-11-05 15:34:09 -0500

    Kaeon

---
+++ Kaeon FUSION/APIs/Updater/Source/aether_updater/Aether.java
@@ -0,0 +1,83 @@
+package aether_updater;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+
+import io.IO;
+import one.Element;
+import one_plus.ONEPlus;
+import updater.Updater;
+
+public class Aether {
+	
+	public static Object onCall(Object object) {
+		
+		try {
+			
+			Element update = ONEPlus.parseONEPlus(IO.openAsString("" + object));
+			Element previousUpdates = Updater.getPreviousUpdates();
+			
+			return Updater.isUpdateAvailable(update, previousUpdates);
+		}
+		
+		catch(Exception exception) {
+			
+		}
+		
+		return false;
+	}
+	
+	public static Integer id;
+	
+	public static void assignID() {
+		
+		if(id != null)
+			return;
+	}
+	
+	public static ArrayList<Integer> getActiveModuleIDs(String name) {
+		
+		ArrayList<Integer> ids = new ArrayList<Integer>();
+		
+		return ids;
+	}
+	
+	public static Object call(String name, int id, Object argument) {
+		return id == 0 ? staticCall(name, argument) : activeCall(name, id, argument);
+	}
+	
+	public static Object staticCall(String name, Object argument) {
+		
+		try {
+			
+			URLClassLoader loader = new URLClassLoader(new URL[] {new URL("file:" + name + ".jar")});
+			
+			String formattedName = name.toLowerCase();
+			
+			for(int i = 0; i < formattedName.length(); i++) {
+				
+				if(formattedName.charAt(i) == ' ')
+					formattedName = formattedName.substring(0, i) + '_' + formattedName.substring(i + 1);
+			}
+			
+			Class<?> aether = Class.forName("aether_" + formattedName + ".Aether", true, loader);
+			Method onCall = aether.getMethod("onCall", new Object().getClass());
+			
+			Object object = onCall.invoke(aether.getConstructor().newInstance(), argument);
+			
+			loader.close();
+			
+			return object;
+		}
+		
+		catch(Exception exception) {
+			return null;
+		}
+	}
+	
+	public static Object activeCall(String name, int id, Object argument) {
+		return null;
+	}
+}
---
+++ Kaeon FUSION/APIs/Updater/Source/updater/Updater.java
@@ -0,0 +1,139 @@
+package updater;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import io.IO;
+import one.Element;
+import one.ElementUtilities;
+import one_plus.ONEPlus;
+
+public class Updater {
+	
+	public static void main(String[] args) {
+		
+		Element update = ONEPlus.parseONEPlus(IO.openAsString(args[0]));
+		
+		ArrayList<String> paths = new ArrayList<String>();
+		
+		for(int i = 1; i < args.length; i++)
+			paths.add(args[i]);
+		
+		Element previousUpdates = Updater.getPreviousUpdates();
+		
+		update(update, paths, previousUpdates);
+	}
+	
+	public static Element getPreviousUpdates() {
+		
+		String previousUpdates = IO.openAsString("Update.op");
+		
+		if(previousUpdates == null)
+			return new Element();
+		
+		return ONEPlus.parseONEPlus(previousUpdates);
+	}
+	
+	public static void update(Element update, ArrayList<String> paths, Element previousUpdates) {
+		
+		for(Element child : update.children)
+			updateModule(child, paths, previousUpdates);
+		
+		updateRecord(update);
+	}
+	
+	public static void updateModule(Element update, ArrayList<String> paths, Element previousUpdates) {
+		
+		if(!isModuleUpdateAvailable(update, previousUpdates))
+			return;
+		
+		boolean exists = ElementUtilities.hasChild(previousUpdates, update.content);
+		
+		if(exists) {
+			
+			for(String path : paths) {
+				
+				if(new File(path + update.content).exists()) {
+					
+					updateModule(update.content, getModuleSource(update), path);
+					
+					return;
+				}
+			}
+		}
+		
+		updateModule(update.content, getModuleSource(update), "");
+	}
+	
+	public static String getModuleSource(Element update) {
+		
+		if(ElementUtilities.hasChild(update, "Source")) {
+			
+			if(ElementUtilities.getChild(update, "Source").children.size() >= 1)
+				return ElementUtilities.getChild(update, "Source").children.get(0).content;
+		}
+		
+		return null;
+	}
+	
+	public static void updateModule(String moduleName, String source, String path) {
+		
+		if(source == null)
+			return;
+		
+		IO.download(source, path + moduleName);
+	}
+	
+	public static void updateRecord(Element update) {
+		
+		Element updateRecord = getPreviousUpdates();
+		
+		for(Element child : update.children)
+			updateModuleRecord(updateRecord, child);
+		
+		IO.save("" + updateRecord, "Update.op");
+	}
+	
+	public static void updateModuleRecord(Element updateRecord, Element update) {
+		
+		ElementUtilities.removeChildren(updateRecord, update.content);
+		
+		ElementUtilities.addChild(updateRecord, ElementUtilities.copyElement(update));
+	}
+	
+	public static boolean isUpdateAvailable(Element update, Element previousUpdates) {
+		
+		for(Element child : update.children) {
+		
+			if(isModuleUpdateAvailable(child, previousUpdates))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	public static boolean isModuleUpdateAvailable(Element update, Element previousUpdates) {
+		
+		if(!ElementUtilities.hasChild(previousUpdates, update.content))
+			return true;
+		
+		try {
+
+			Element previousDate = ElementUtilities.getChild(previousUpdates, update.content);
+			previousDate = ElementUtilities.getChild(previousDate, "Date").children.get(0);
+			
+			if(previousDate == null)
+				return true;
+			
+			Element date = ElementUtilities.getChild(update, "Date").children.get(0);
+			
+			return !previousDate.content.equals(date.content);
+		}
+		
+		catch(Exception exception) {
+			
+		}
+		
+		return false;
+	}
+}
