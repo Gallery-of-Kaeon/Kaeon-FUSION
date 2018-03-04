65cfd3f58e5f7d45b45ab39673807e301719cab4
Kipmon <mdinkin54@gmail.com>
2018-03-03 22:16:54 -0500

    Kaeon

--- Kaeon FUSION/Interfaces/Stack/Source/stack/dialects/JSON.java
+++ Kaeon FUSION/Interfaces/Stack/Source/stack/dialects/JSON.java
@@ -1,7 +1,146 @@
 package stack.dialects;
 
+import java.util.ArrayList;
+
+import one.Element;
 import stack.utilities.Dialect;
 
 public class JSON extends Dialect {
+	
+	public void build(
+			ArrayList<ArrayList<String>> files,
+			ArrayList<Element> code,
+			String name,
+			int index,
+			ArrayList<Object> arguments) {
+		
+		Element element = code.get(0);
+		
+		String json = "";
+		
+		for(int i = 0; i < element.children.size(); i++)
+			json += processElement(element.children.get(i));
+		
+		ArrayList<String> file = new ArrayList<String>();
+		
+		if(name == null) {
+			
+			name = "data";
+			
+			if(index > 0)
+				name += "_" + index;
+		}
+		
+		file.add(name + ".json");
+		file.add(json);
+		
+		files.add(file);
+	}
+	
+	public String processElement(Element element) {
+		
+		if(element.content.equalsIgnoreCase("List")) {
+			
+			String list = "[";
+			
+			for(int i = 0; i < element.children.size(); i++) {
+				
+				list += processElement(element.children.get(i));
+				
+				if(i < element.children.size() - 1)
+					list += ",";
+			}
+			
+			return list + "]";
+		}
+		
+		if(element.content.equalsIgnoreCase("Object")) {
+			
+			String list = "{";
+			
+			for(int i = 0; i < element.children.size(); i++) {
+				
+				list +=
+						processFieldName(element.children.get(i)) +
+						":" +
+						processElement(element.children.get(i).children.get(0));
+				
+				if(i < element.children.size() - 1)
+					list += ",";
+			}
+			
+			return list + "}";
+		}
+		
+		return processLiteral(element);
+	}
+	
+	public String processLiteral(Element element) {
+		
+		String literal = element.content;
+		
+		try {
+			
+			if(!(literal.startsWith("\"") && literal.endsWith("\""))) {
+				
+				Double.parseDouble(literal);
+			
+				return literal;
+			}
+		}
+		
+		catch(Exception exception) {
+			
+		}
+		
+		if(literal.equalsIgnoreCase("true") ||
+				literal.equalsIgnoreCase("false") ||
+				literal.equalsIgnoreCase("null")) {
+			
+			return literal;
+		}
+		
+		if(literal.startsWith("\""))
+			literal = literal.substring(1);
+		
+		if(literal.endsWith("\""))
+			literal = literal.substring(0, literal.length() - 1);
 
+		literal.replace("\\", "\\\\");
+		literal.replace("\"", "\\\"");
+		literal.replace("\b", "\\b");
+		literal.replace("\f", "\\f");
+		literal.replace("\n", "\\n");
+		literal.replace("\r", "\\r");
+		literal.replace("\t", "\\t");
+		
+		return "\"" + literal + "\"";
+	}
+	
+	public String processFieldName(Element element) {
+		
+		String literal = element.content;
+		
+		if(literal.length() > 0) {
+			
+			if(literal.startsWith("\""))
+				literal = literal.substring(1);
+		}
+		
+		if(literal.length() > 0) {
+			
+			if(literal.endsWith("\""))
+				literal = literal.substring(0, literal.length() - 1);
+		}
+
+		literal.replace("\\", "\\\\");
+		literal.replace("\"", "\\\"");
+		literal.replace("\b", "\\b");
+		literal.replace("\f", "\\f");
+		literal.replace("\n", "\\n");
+		literal.replace("\r", "\\r");
+		literal.replace("\t", "\\t");
+		
+		return "\"" + literal + "\"";
+	}
 }
--- Kaeon FUSION/Interfaces/Stack/Source/stack/dialects/XML.java
+++ Kaeon FUSION/Interfaces/Stack/Source/stack/dialects/XML.java
@@ -1,7 +1,91 @@
 package stack.dialects;
 
+import java.util.ArrayList;
+
+import one.Element;
 import stack.utilities.Dialect;
 
 public class XML extends Dialect {
-
+	
+	public void build(
+			ArrayList<ArrayList<String>> files,
+			ArrayList<Element> code,
+			String name,
+			int index,
+			ArrayList<Object> arguments) {
+		
+		Element element = code.get(0);
+		
+		String xml = "";
+		
+		for(int i = 0; i < element.children.size(); i++)
+			xml += processElement(element.children.get(i));
+		
+		ArrayList<String> file = new ArrayList<String>();
+		
+		if(name == null) {
+			
+			name = "data";
+			
+			if(index > 0)
+				name += "_" + index;
+		}
+		
+		file.add(name + ".xml");
+		file.add(xml);
+		
+		files.add(file);
+	}
+	
+	public String processElement(Element element) {
+		
+		String xml = "<" + processLiteral(element) + " ";
+		
+		for(int i = 0; i < element.children.size(); i++) {
+			
+			if(element.children.get(i).content.equalsIgnoreCase("Attributes")) {
+				
+				for(int j = 0; j < element.children.get(i).children.size(); j++) {
+					
+					xml +=
+							processLiteral(element.children.get(i).children.get(j)) +
+							"=\"" +
+							processLiteral(element.children.get(i).children.get(j).children.get(0)) +
+							"\"";
+				}
+			}
+		}
+		
+		xml += ">";
+		
+		for(int i = 0; i < element.children.size(); i++) {
+			
+			if(element.children.get(i).content.equalsIgnoreCase("Data")) {
+				
+				for(int j = 0; j < element.children.get(i).children.size(); j++)
+					xml += processLiteral(element.children.get(i).children.get(j));
+			}
+			
+			if(element.children.get(i).content.equalsIgnoreCase("Children")) {
+				
+				for(int j = 0; j < element.children.get(i).children.size(); j++)
+					xml += processElement(element.children.get(i).children.get(j));
+			}
+		}
+		
+		return xml + "</" + processLiteral(element) + ">";
+	}
+	
+	public String processLiteral(Element element) {
+		
+		String literal = element.content;
+		
+		literal.replace("&", "&amp;");
+		literal.replace("<", "&lt;");
+		literal.replace(">", "&gt;");
+		literal.replace("\"", "&quot;");
+		literal.replace("\'", "&apos;");
+		
+		return literal;
+	}
 }
