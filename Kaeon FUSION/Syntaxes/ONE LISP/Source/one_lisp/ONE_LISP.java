ea4ffae0c88af51ac99fb1d1dfcbccf9f43c620d
Kipmon <mdinkin54@gmail.com>
2018-04-10 15:21:05 -0400

    Kaeon

---
+++ Kaeon FUSION/Syntaxes/ONE LISP/Source/one_lisp/ONE_LISP.java
@@ -0,0 +1,93 @@
+package one_lisp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import one.Element;
+import one.ElementUtilities;
+import one_plus.directive.Directive;
+
+public class ONE_LISP extends Directive {
+	
+	public static Element parse(String string) {
+		
+		Element one = new Element();
+		
+		ArrayList<String> tokens =
+				Tokenizer.tokenize(
+						new ArrayList<String>(
+								Arrays.asList(
+										" ", "\t", "\n", "\"", "(", ")", "\\")),
+						string);
+		
+		Element currentElement = one;
+		
+		boolean isQuote = false;
+		String literal = "";
+		
+		for(int i = 0; i < tokens.size(); i++) {
+			
+			if(tokens.get(i).equals("\"")) {
+				
+				isQuote = !isQuote;
+				
+				if(!isQuote && literal.length() > 0) {
+					
+					if(currentElement.content == null)
+						currentElement.content = literal;
+					
+					else
+						ElementUtilities.addChild(currentElement, ElementUtilities.createElement(literal));
+					
+					literal = "";
+				}
+				
+				continue;
+			}
+			
+			if(tokens.get(i).equals("\\") && i < tokens.size() - 1) {
+				
+				if(isQuote)
+					literal += tokens.get(i + 1);
+				
+				else {
+					
+					if(currentElement.content == null)
+						currentElement.content = tokens.get(i + 1);
+					
+					else
+						ElementUtilities.addChild(currentElement, ElementUtilities.createElement(tokens.get(i + 1)));
+				}
+				
+				i++;
+				
+				continue;
+			}
+			
+			if(tokens.get(i).equals("(") && i < tokens.size() - 1) {
+				
+				Element newElement = new Element();
+				ElementUtilities.addChild(currentElement, newElement);
+				
+				currentElement = newElement;
+			}
+			
+			else if(tokens.get(i).equals(")") && currentElement.parent != null)
+				currentElement = currentElement.parent;
+			
+			else if(isQuote)
+				literal += tokens.get(i);
+			
+			else if(!tokens.get(i).equals(" ") && !tokens.get(i).equals("\n") && !tokens.get(i).equals("\t")) {
+				
+				if(currentElement.content == null)
+					currentElement.content = tokens.get(i);
+				
+				else
+					ElementUtilities.addChild(currentElement, ElementUtilities.createElement(tokens.get(i)));
+			}
+		}
+		
+		return one;
+	}
+}
