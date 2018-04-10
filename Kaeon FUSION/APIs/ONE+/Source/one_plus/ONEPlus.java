ef73f52c589b4461efa2d1d12a303221122614f5
Kipmon <mdinkin54@gmail.com>
2018-04-09 21:28:59 -0400

    Kaeon

--- Kaeon FUSION/APIs/ONE+/Source/one_plus/ONEPlus.java
+++ Kaeon FUSION/APIs/ONE+/Source/one_plus/ONEPlus.java
@@ -2,6 +2,7 @@ package one_plus;
 
 import java.util.ArrayList;
 
+import aether_one_plus.Aether;
 import one.Element;
 import one_plus.parse.Preprocessor;
 import one_plus.parse.Processor;
@@ -14,6 +15,23 @@ public class ONEPlus {
 	public static PhilosophersStone host;
 	
 	public static Element parseONEPlus(String string) {
+		
+		if(string.trim().startsWith("-[")) {
+			
+			try {
+				
+				return
+						(Element)
+						Aether.call(
+								string.substring(string.indexOf("-[") + 2, string.indexOf("]\n")),
+								0,
+								string.substring(string.indexOf("]\n") + 2));
+			}
+			
+			catch(Exception exception) {
+				
+			}
+		}
 		
 		ArrayList<String> tokens = TokenGenerator.getTokens(string);
 		ArrayList<String> tokenize = Tokenizer.tokenize(tokens, string);
