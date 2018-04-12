876cc26adbd4414ebbcb50107d36afae7f7a84ac
Kipmon <mdinkin54@gmail.com>
2018-04-12 15:08:29 -0400

    Kaeon

---
+++ Kaeon FUSION/Interfaces/Standard/Source/standard_kaeon_fusion/commands/list/ListToElement.java
@@ -0,0 +1,46 @@
+package standard_kaeon_fusion.commands.list;
+
+import java.util.ArrayList;
+
+import fusion.FUSIONUnit;
+import one.Element;
+import one.ElementUtilities;
+
+public class ListToElement extends FUSIONUnit {
+	
+	public ListToElement() {
+		tags.add("Standard");
+	}
+	
+	public boolean verify(Element element) {
+		return element.content.equalsIgnoreCase("List to Element");
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Object process(Element element, ArrayList<Object> processed) {
+		return "" + listToElement((ArrayList<Object>) processed.get(0), false);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Element listToElement(ArrayList<Object> list, boolean content) {
+		
+		Element element =
+				content ?
+						ElementUtilities.createElement(
+								list.size() > 0 ?
+										"" + list.get(0) :
+										"") :
+						new Element();
+		
+		for(int i = content ? 1 : 0; i < list.size(); i++) {
+			
+			if(list.get(i) instanceof ArrayList)
+				ElementUtilities.addChild(element, listToElement((ArrayList<Object>) list.get(i), true));
+			
+			else
+				ElementUtilities.addChild(element, ElementUtilities.createElement("" + list.get(i)));
+		}
+		
+		return element;
+	}
+}
