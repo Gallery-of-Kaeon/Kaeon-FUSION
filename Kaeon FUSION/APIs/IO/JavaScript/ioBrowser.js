3c6c36fc56ab6a6a980a56a6143af2e47b60946f
Jesse Dinkin <kaeon.ace@gmail.com>
2019-06-26 22:01:16 -0400

    Kaeon

---
+++ Kaeon FUSION/APIs/IO/JavaScript/ioBrowser.js
@@ -0,0 +1,54 @@
+function getInput(query) {
+
+	if(query != null)
+		console.log(query);
+
+	return readline();
+}
+
+function open(file) {
+
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", file, false);
+
+	var allText = "";
+
+	rawFile.onreadystatechange = function() {
+
+		if(rawFile.readyState === 4) {
+
+			if(rawFile.status === 200 || rawFile.status == 0) {
+				allText = rawFile.responseText;
+			}
+		}
+	}
+
+	rawFile.send(null);
+
+	return allText;
+}
+
+function save(content, file) {
+
+	let element = document.createElement('a');
+
+	element.setAttribute(
+		'href',
+		'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+
+	element.setAttribute('download', file);
+
+	element.style.display = 'none';
+	document.documentElement.appendChild(element);
+
+	element.click();
+
+	document.documentElement.removeChild(element);
+}
+
+module.exports = {
+
+	getInput,
+	open,
+	save
+};
